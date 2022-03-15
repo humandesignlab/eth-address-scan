18b05c5704cbe084ff010ee0c50f5899d61f5176
Humberto Mangino <hmangino@gmail.com>
2022-03-15 13:47:11 -0700

    add scan

--- src/eth-comb.js
+++ src/eth-comb.js
@@ -3,8 +3,10 @@ import Web3 from 'web3';
 const web3 = new Web3('ws://localhost:8545');
 
 import * as fs from 'fs';
-let usersjson = fs.readFileSync('accounts.json', 'utf-8');
-let targets = JSON.parse(usersjson);
+let accountsjson = fs.readFileSync('accounts.json', 'utf-8');
+let accounts = JSON.parse(accountsjson);
+let targetsjson = fs.readFileSync('targets.json', 'utf-8');
+let targets = JSON.parse(targetsjson);
 
 //const targets = [];
 let counter = 0;
@@ -13,7 +15,7 @@ export async function comb() {
   console.log(counter);
   for (let i = 0; i < 100; i++) {
     const pk = crypto.randomBytes(32).toString('hex');
-    const addresFromPk = web3.eth.accounts.privateKeyToAccount(pk);
+    const addresFromPk = web3.eth.accounts.privateKeyToAccount(`0x${pk}`);
     const addressBalance = await web3.eth.getBalance(addresFromPk.address);
 
     if (addressBalance !== '0') {
@@ -22,10 +24,17 @@ export async function comb() {
         address: addresFromPk.address,
         balance: addressBalance,
       });
-      usersjson = JSON.stringify(targets);
-      fs.writeFileSync('accounts.json', usersjson, 'utf-8');
+      targetsjson = JSON.stringify(targets);
+      fs.writeFileSync('targets.json', targetsjson, 'utf-8');
       console.log('Fish caught!!');
     }
+    accounts.push({
+      privateKey: pk,
+      address: addresFromPk.address,
+      balance: addressBalance,
+    });
+    accountsjson = JSON.stringify(accounts);
+    fs.writeFileSync('accounts.json', accountsjson, 'utf-8');
   }
 
   setTimeout(comb, 3000);
