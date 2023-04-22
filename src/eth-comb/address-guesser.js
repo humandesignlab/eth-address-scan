b0865e5f3bab3f90815b358ecf0ffe792a427a64
humandesignlab <hmangino@gmail.com>
2023-04-22 10:11:51 -0600

    Refactor algorithm

--- src/eth-comb/address-guesser.js
+++ src/eth-comb/address-guesser.js
@@ -2,28 +2,30 @@ import generateAccount from "./account-generator.js";
 import topAccounts from "../input-data/scrape-nov-22.js";
 
 function guessAnAddress() {
+  const topAccountsMap = new Map();
+
+  topAccounts.forEach((item, index) =>
+    topAccountsMap.set(item.toLocaleUpperCase(), index)
+  );
+
   for (let i = 1; i <= 300000000; i++) {
     let account = generateAccount();
 
-    let index = topAccounts.findIndex(
-      (item) => item.toUpperCase() === account.address.toUpperCase()
-    );
+    // if (i === 4567) account.address = '0xa04d81f5c75cc159a72548caed8bb77192715bc8'
 
-    let isInArray = topAccounts.find(
-      (item) => item.toUpperCase() === account.address.toUpperCase()
-    );
-    // if (index !== -1) {
-    //   console.log("Found!", index);
-    //   console.log({ address: account.address, PK: account.privateKey, index })
-    //   return { address: account.address, PK: account.privateKey, index };
-    // }
-    if (isInArray) {
-      console.log("Found!");
-      console.log({ address: account.address, PK: account.privateKey, index });
-      return { address: account.address, PK: account.privateKey, index };
+    if (topAccountsMap.has(account.address.toUpperCase())) {
+      console.log({
+        address: account.address,
+        pk: account.privateKey,
+        index: topAccountsMap.get(account.address.toUpperCase()),
+      });
+      return {
+        address: account.address,
+        pk: account.privateKey,
+        index: topAccountsMap.get(account.address.toUpperCase()),
+      };
     }
-
-    console.log(i, account.address);
+    i % 1000 === 0 ? console.log(i, account.address) : null;
   }
 }
 guessAnAddress();
