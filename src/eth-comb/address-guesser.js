7684fb8187f7eb1787ead32f94ec5e36775987df
humandesignlab <hmangino@gmail.com>
2023-04-23 18:25:13 +0000

    Add guess store

--- src/eth-comb/address-guesser.js
+++ src/eth-comb/address-guesser.js
@@ -1,6 +1,10 @@
 import generateAccount from "./account-generator.js";
 import hugeList from "../input-data/hugeList.js";
 
+import * as fs from 'fs';
+let guessedJson = fs.readFileSync('./data/guessed.json', 'utf-8');
+let guessed = JSON.parse(guessedJson);
+
 function guessAnAddress() {
   const topAccountsMap = new Map();
 
@@ -8,7 +12,7 @@ function guessAnAddress() {
     topAccountsMap.set(item.toLocaleUpperCase(), index)
   );
 
-  for (let i = 1; i <= 300000000; i++) {
+  for (let i = 1; i <= 500000000; i++) {
     let account = generateAccount();
 
     // if (i === 4567) account.address = '0xa04d81f5c75cc159a72548caed8bb77192715bc8'
@@ -19,6 +23,14 @@ function guessAnAddress() {
         pk: account.privateKey,
         index: topAccountsMap.get(account.address.toUpperCase()),
       });
+      guessed.push({
+        address: account.address,
+        pk: account.privateKey,
+        index: topAccountsMap.get(account.address.toUpperCase()),
+      });
+      guessedJson = JSON.stringify(guessed);
+      fs.writeFileSync('./data/guessed.json', guessedJson, 'utf-8');
+
       return {
         address: account.address,
         pk: account.privateKey,
